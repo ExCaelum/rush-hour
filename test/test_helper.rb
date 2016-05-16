f70faf48aee828d2c1905ee538dc77843835e287
Parker P <exxcaelum@gmail.com>
2016-05-16 15:45:08 -0600

    set up the database_cleaner gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,5 +7,9 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'capybara/dsl'
+require 'database_cleaner'
+
 
 Capybara.app = RushHour::Server
+DatabaseCleaner.strategy = :truncation
+# DatabaseCleaner.clean
