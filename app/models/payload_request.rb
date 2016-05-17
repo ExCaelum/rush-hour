433a6041b29ce11a87ecb4326248c1d7686ad19e
Parker P <exxcaelum@gmail.com>
2016-05-17 13:18:44 -0600

    updated validations

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -1,6 +1,12 @@
 class PayloadRequest < ActiveRecord::Base
-  validates :url, :requested_at, :responded_in, :referred_by, :request_type,
-  :parameters, :event_name, :user_agent, :resolution_width, :resolution_height,
-  :ip,
-  :presence => true
+  validates :url, :presence => true
+  validates :requested_at, :presence => true
+  validates :responded_in, :presence => true
+  validates :referred_by, :presence => true
+  validates :request_type, :presence => true
+  validates :event_name, :presence => true
+  validates :user_agent, :presence => true
+  validates :resolution_width, :presence => true
+  validates :resolution_height, :presence => true
+  validates :ip, :presence => true
 end
