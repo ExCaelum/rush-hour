cc6652f4b51961afe2dd65b48e59df9c0f491b42
saylerb <sayler.b@gmail.com>
2016-05-22 18:25:26 -0600

    add test for bad 24 hour time format

--- test/controllers/receive_client_payload_test.rb
+++ test/controllers/receive_client_payload_test.rb
@@ -58,4 +58,14 @@ class ReceiveClientPayloadTest < Minitest::Test
     assert_equal 1, PayloadRequest.count
     assert_equal 200, last_response.status
   end
+
+  def test_it_stores_times_greater_than_24
+    Client.create(identifier: "Client1", root_url: "www.client.com")
+    payload = '{"url":"http://jumpstartlab.com/blog","requestedAt":"2013-02-16 24:38:28 -0700","respondedIn":37,"referredBy":"http://jumpstartlab.com","requestType":"GET","parameters":[],"eventName":"socialLogin","userAgent":"Mozilla/5.0 (Macintosh%3B Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17","resolutionWidth":"1920","resolutionHeight":"1280","ip":"63.29.38.211"}'
+
+    post '/sources/Client1/data', {payload: payload}
+    assert_equal 418, last_response.status
+    assert_equal "Bad Data", last_response.body
+  end
+
 end
