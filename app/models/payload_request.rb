450644bd84506a101355f6ab60379d120851e26f
saylerb <sayler.b@gmail.com>
2016-05-22 23:45:13 -0600

    adjust indentation

--- app/models/payload_request.rb
+++ app/models/payload_request.rb
@@ -64,18 +64,19 @@ class PayloadRequest < ActiveRecord::Base
     inclusive_payload[:client] = client
 
     pr = PayloadRequest.new
-    pr.attributes = { requested_at:  payload[:requested_at],
-                      responded_in:  payload[:responded_in],
-                      referrer: Referrer.where(payload[:referrer]).first_or_create,
-                      request_type: RequestType.where(payload[:request_type]).first_or_create,
-                      event_name: EventName.where(payload[:event_name]).first_or_create,
-                      resolution: Resolution.where(payload[:resolution]).first_or_create,
-                      user_agent: UserAgent.where(payload[:user_agent]).first_or_create,
-                      ip: Ip.where(payload[:ip]).first_or_create,
-                      url: Url.where(payload[:url]).first_or_create,
-                      client: client,
-                      parameters: payload[:parameters],
-                      key: PayloadParser.generate_sha(inclusive_payload) }
+    pr.attributes = 
+      { requested_at:  payload[:requested_at],
+        responded_in:  payload[:responded_in],
+        referrer: Referrer.where(payload[:referrer]).first_or_create,
+        request_type: RequestType.where(payload[:request_type]).first_or_create,
+        event_name: EventName.where(payload[:event_name]).first_or_create,
+        resolution: Resolution.where(payload[:resolution]).first_or_create,
+        user_agent: UserAgent.where(payload[:user_agent]).first_or_create,
+        ip: Ip.where(payload[:ip]).first_or_create,
+        url: Url.where(payload[:url]).first_or_create,
+        client: client,
+        parameters: payload[:parameters],
+        key: PayloadParser.generate_sha(inclusive_payload) }
     pr.save
   end
 
